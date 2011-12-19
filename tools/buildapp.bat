83127b67e7ee014afb6b8282faf590434e73009e
Simon Crabtree <simon@simoncrabtree.net>
2011-12-19 13:57:09 +0000

    Corrected error in Windows build script

--- tools/buildapp.bat
+++ tools/buildapp.bat
@@ -1,11 +1,11 @@
-rmdir /S/Q ../../dojo-reference-release
+rmdir /S/Q ..\..\dojo-reference-release
 
-pushd ..\src\js\util\buildscripts
+@pushd ..\src\js\util\buildscripts
 
 call build.bat  --profile ..\..\..\..\tools\release.profile.js --release
 
-popd
+@popd
 
-copy ..\src\index.html ..\..\dojo-reference-release\index.html
-copy ..\src\js\config.js ..\..\dojo-reference-release\js\config.js
+@copy ..\src\index.html ..\..\dojo-reference-release\index.html
+@copy ..\src\js\config.js ..\..\dojo-reference-release\js\config.js
 
