87138f7bfdf4eec6ed034edf7659167458a32d04
Simon Crabtree <simon@simoncrabtree.net>
2011-12-23 11:29:14 +0000

    Added Jasmine into the tools directory and created a modified version of the Jasmine spec runner html page which runs an initial "Smoketest" spec.

---
+++ src/js/specs/smoketest.js
@@ -0,0 +1,9 @@
+describe("Smoke Test", function(){
+  it("should pass", function(){
+    expect(true).toBe(true);
+  });
+  
+  it("should fail", function(){
+    expect(true).toBe(false);
+  });
+});
