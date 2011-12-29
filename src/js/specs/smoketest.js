7fdc4b2dca1e86e25acec8a21ad7206b1731281c
Simon Crabtree <simon@simoncrabtree.net>
2011-12-29 11:07:10 +0000

    Removed incorrect Script tag in index.html Modified jasmine specrunner to work with Dojo AMD

--- src/js/specs/smoketest.js
+++ src/js/specs/smoketest.js
@@ -1,9 +1,5 @@
 describe("Smoke Test", function(){
-  it("should pass", function(){
-    expect(true).toBe(true);
-  });
-  
-  it("should fail", function(){
-    expect(true).toBe(false);
-  });
+    it("should pass", function () {
+        expect(true).toBe(true);
+    });
 });
